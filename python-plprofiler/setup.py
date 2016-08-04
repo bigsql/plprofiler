0cfe8504fa34b685da73e82827345c0b0cf117d8
Jan Wieck <jan@wi3ck.info>
2016-08-03 20:54:57 -0400

    Setting release tag in python-plprofiler to 3-BETA3. Adding psycopg2 install dependency.

--- python-plprofiler/setup.py
+++ python-plprofiler/setup.py
@@ -3,12 +3,15 @@ from setuptools import setup
 setup(
     name = 'plprofiler',
     description = 'PL/pgSQL Profiler command line tool',
-    version = '2.0',
+    version = '3-BETA3',
     author = 'Jan Wieck',
     author_email = 'janw@openscg.com',
     url = 'https://bitbucket.org/openscg/plprofiler/overview',
     license = 'Artistic License',
     packages = ['plprofiler', ],
+    install_requires = [
+        'psycopg2',
+    ],
     entry_points = {
         'console_scripts': [
             'plprofiler = plprofiler:main',
