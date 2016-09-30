9d43313472c77eda43ca7cf979a8692f8c8939a0
Jan Wieck <jan@wi3ck.info>
2016-09-30 10:59:27 -0400

    With REL3_STABLE being tagged as REL3_0 for GA the master branch is now 4_DEVEL.

--- python-plprofiler/setup.py
+++ python-plprofiler/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'plprofiler',
     description = 'PL/pgSQL Profiler command line tool',
-    version = '3-BETA3',
+    version = '4-DEV',
     author = 'Jan Wieck',
     author_email = 'janw@openscg.com',
     url = 'https://bitbucket.org/openscg/plprofiler/overview',
